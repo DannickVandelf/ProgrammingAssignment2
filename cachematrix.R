bb816a122d961f08e94ac931ae00aeeff680f38e
DannickVandelf <d.ngoupse@gmail.com>
2017-02-03 21:43:09 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+        
+        #getters and setters of x and inv vars
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() m
+
+        setInv <- function(inverse) inv <<- inverse 
+        
+        getInv <- function() inv
+        
+        l <- list(set = set, get = get, 
+                setInv = setInv,
+                getInv = getInv)
+        return(l)
 
 }
 
@@ -12,4 +30,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         inv <- matrix$getInv()
+        
+        if(!is.null(inv)) {
+                message("Getting the computed value of inv")
+                return(inv)
+        }
+        
+        
+        data <- matrix$get()
+        
+        if(det(data) == 0) {
+              stop("The matrix has no inverse because det = 0")  
+        } else {
+                inv <- solve(data)
+                matrix$setInv(inv)
+        }
+        
+        inv
 }
